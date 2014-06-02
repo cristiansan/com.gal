ddd4395d25896356e2804d5763f32e3e4caac5ae
ITX <ITX@ITX-PC>
2014-06-01 23:39:56 -0300

    creating act

---
+++ src/github/app/FunctionActivity.java
@@ -0,0 +1,15 @@
+package github.app;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class FunctionActivity extends Activity
+{
+	@Override
+	protected void onCreate(Bundle savedInstanceState){
+		super.onCreate(savedInstanceState);
+		
+		setContentView(R.layout.activity_function);
+	}
+
+}
