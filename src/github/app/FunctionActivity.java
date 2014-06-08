f835cc970dff32792bc7d3cd2c9d9230b0375da0
ITX <ITX@192.168.0.204>
2014-06-08 15:53:38 -0300

    clock button

--- src/github/app/FunctionActivity.java
+++ src/github/app/FunctionActivity.java
@@ -1,7 +1,11 @@
 package github.app;
 
 import android.app.Activity;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.DigitalClock;
+import android.widget.TextView;
 
 public class FunctionActivity extends Activity
 {
@@ -10,6 +14,22 @@ public class FunctionActivity extends Activity
 		super.onCreate(savedInstanceState);
 		
 		setContentView(R.layout.activity_function);
-	}
+		
+		final DigitalClock dc=(DigitalClock) findViewById(R.id.digitalClock1);
+		final TextView tv=(TextView) findViewById(R.id.act_function_tittle_top);
 
+		dc.setClickable(true);
+		dc.setOnClickListener(new View.OnClickListener() {
+			
+		@Override
+		public void onClick(View v) {
+		tv.setText(dc.getText().toString());
+		tv.setTextColor(Color.RED);
+		tv.setTextSize(50);
+		
+		}
+		});
+		}
+
+	
 }
